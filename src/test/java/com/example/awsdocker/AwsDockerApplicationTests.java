185cef2f23611dd0b912ec66fe4f21bfd8b6c4d0
hyosoek <tennfin1@gmail.com>
2024-11-15 16:21:50 +0900

    init

---
+++ src/test/java/com/example/awsdocker/AwsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
